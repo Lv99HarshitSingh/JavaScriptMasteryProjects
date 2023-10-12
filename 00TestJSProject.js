e408dd2da0851ba21f22ab5d331368c52411c1e5
Lv99HarshitSingh <singh.harshit2922@gmail.com>
2023-10-12 20:23:19 +0530

    Completed 00 project

--- 00TestJSProject.js
+++ 00TestJSProject.js
@@ -1,1 +1,14 @@
 console.log("Hello Universe!")
+let count = 0
+document.getElementById("increase").onclick = function() {
+    count += 1
+    document.getElementById("counter").innerHTML = count
+}
+document.getElementById("decrease").onclick = function() {
+    count -= 1
+    document.getElementById("counter").innerHTML = count
+}
+document.getElementById("reset").onclick = function() {
+    count = 0
+    document.getElementById("counter").innerHTML = count
+}
